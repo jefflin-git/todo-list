a449fd241499e5bbb444d0ee525fcdf03f8afeb1
jefflin <linjef0711@gmail.com>
2021-01-09 19:52:41 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,16 @@ const router = express.Router()
 const home = require('./modules/home.js')
 // 引入 todos 模組程式碼
 const todos = require('./modules/todos.js')
+// 引入 users 模組程式碼
+const users = require('./modules/users.js')
 
 // 準備引入路由模組
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
 router.use('/todos', todos)
+// 將網址結構符合 /users 字串開頭的 request 導向 users 模組
+router.use('/users', users)
 
 // 匯出路由器
 module.exports = router
