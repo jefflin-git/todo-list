cc6a76a342a7042e8e8a2a0c0988f0f4644a7c10
jefflin <linjef0711@gmail.com>
2021-01-11 11:10:39 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const methodOverride = require('method-override')
 // 引用路由器
 const routes = require('./routes/index.js')
 const session = require('express-session')
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
 // 如果在 Heroku 環境則使用 process.env.PORT
 // 否則為本地環境，使用 3000 
 const PORT = process.env.PORT || 3000
@@ -32,6 +34,10 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
+
 // 將 request 導入路由器
 app.use(routes)
 
